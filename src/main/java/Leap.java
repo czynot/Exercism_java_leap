aaf7c7590ba14916433d97ffb607b7b425fd735c
Parth Purvesh <parth29dec@gmail.com>
2023-10-10 11:52:03 +0530

    Tests passed

--- src/main/java/Leap.java
+++ src/main/java/Leap.java
@@ -1,7 +1,8 @@
 class Leap {
 
     boolean isLeapYear(int year) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+//        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return year % 400 == 0 || (year % 4 == 0 && year % 100 != 0);
     }
 
 }
